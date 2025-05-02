6ca4997a6ee33212d6c5b5984c74cd6cd9a4db80
Birajit Saikia <birajitsaikia@gmail.com>
2025-05-02 17:49:25 +0530

    fixxxx

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable react/no-unescaped-entities */
 import { useEffect, useRef, useState } from 'react';
 import Image from "next/image";
 import styles from "./page.module.css";
